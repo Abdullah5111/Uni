c6d2e08187c00a2d77f4e4f643a8d7d3946c664a
Abdullah <122647095+Abdullah5111@users.noreply.github.com>
2023-08-10 23:55:19 +0500

    frontend app created

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,31 @@
+// src/LoginForm.js
+import React, { useState } from 'react';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Login Form</h2>
+      <div>
+        <label>Username:</label>
+        <input type="text" value={username} onChange={handleUsernameChange} />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="password" value={password} onChange={handlePasswordChange} />
+      </div>
+    </div>
+  );
+}
+
+export default Login;
